fb5d912c534d659901c67af7913da185551dcbd2
HItesh Ramoliya <amarinfotech@HIteshs-Mac-mini.local>
2014-09-29 16:56:48 +0530

    Initial Commit

---
+++ demoProtocol/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  demoProtocol
+//
+//  Created by HItesh Ramoliya on 9/29/14.
+//  Copyright (c) 2014 HItesh Ramoliya. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
